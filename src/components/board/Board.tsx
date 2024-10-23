d4407823db4b1f308477e728b03d5f3b37b7f6f6
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2024-10-23 16:51:17 -0500

    initial commit

---
+++ src/components/board/Board.tsx
@@ -0,0 +1,23 @@
+const Board = () => {
+  return (
+    <>
+    <div className="board_row">
+      <button className="board_cell">1</button>
+      <button className="board_cell">2</button>
+      <button className="board_cell">3</button>
+    </div>
+    <div className="board_row">
+      <button className="board_cell">4</button>
+      <button className="board_cell">5</button>
+      <button className="board_cell">6</button>
+    </div>  
+    <div className="board_row">
+      <button className="board_cell">7</button>
+      <button className="board_cell">8</button>
+      <button className="board_cell">9</button>
+    </div>
+    </>
+  )
+}
+
+export default Board
---
+++ src/components/playerBox/PlayerBox.tsx
@@ -0,0 +1,7 @@
+const PlayerBox = () => {
+  return (
+    <button className="player-box">Player Box</button>
+  )
+}
+
+export default PlayerBox    
